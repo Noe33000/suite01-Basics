33fd0e38f710971509e4e69d35472d8fa32e3e9e
Noe33000 <champignyn@gmail.com>
2017-03-28 11:20:04 +0200

    ex12.js step 1 to 3 done

--- ex12.js
+++ ex12.js
@@ -76,3 +76,102 @@
 */
 
 // write your code below this comment
+function plus(number1, number2){
+    return number1 + number2;
+}
+function moins(number1, number2){
+    return number1 - number2;
+}
+function fois(number1, number2){
+    return number1 * number2;
+}
+function divi(number1, number2){
+    return number1 / number2;
+}
+function rest(number1, number2){
+    return number1 % number2;
+}
+
+function evalExpr(number1, number2, operator){
+    switch (true){        
+        case (operator === '+'):
+            return plus(number1, number2);
+            break;
+        case (operator === '-'):
+            return moins(number1, number2);
+            break;
+        case (operator === '*'):
+            return fois(number1, number2);
+            break;
+        case (number2 == 0 && operator === '/'):
+            return 'Impossible de diviser par 0';
+            break;
+        case (operator === '/'):
+            return divi(number1, number2);
+            break;
+        case (operator === '%'):
+            return rest(number1, number2);
+            break;
+        default:
+            return "Erreur: entrez 2 chiffres et un operateur";
+            break;
+    }
+}
+/*
+    Vérification STEP 1
+
+ console.log(evalExpr(4, 5, "*")); // 20
+ console.log(evalExpr(3, 3, "+")); // 6
+ console.log(evalExpr(2, 2, "%")); // 0
+ console.log(evalExpr(3, 3, "-")); // 0
+ console.log(evalExpr(10, 2, "/")); // 5
+
+ console.log(evalExpr(10, 0, "/")); // 0
+*/
+
+ function getRandNum(){
+     return Math.floor(Math.random() * 10000);
+ }
+ function getSign(){
+     return Math.floor(getRandNum() / 2000);
+ }
+ /*
+    Vérification STEP 2
+
+ console.log(getRandNum());
+ console.log(getSign());
+ */
+
+function generateCalculs(numberOfcalculs){
+    for(i = 0; i < numberOfcalculs; i++){
+        let nbre = getSign();
+        let number1 = getRandNum(); 
+        let number2 = getRandNum();
+        let result = 0; 
+        
+        switch (true){        
+            case (nbre == 0):
+                result = plus(number1, number2); 
+                return result;
+                break;
+            case (nbre == 1):
+                result = moins(number1, number2); 
+                return result;
+                break;
+            case (nbre == 2):
+                result = fois(number1, number2); 
+                return result;
+                break;
+            case (nbre == 3):
+                result = divi(number1, number2); 
+                return result;
+                break;
+            case (nbre == 4):
+                result = rest(number1, number2); 
+                return result;
+                break;
+        }
+    }
+}
+
+console.log(generateCalculs(1));
